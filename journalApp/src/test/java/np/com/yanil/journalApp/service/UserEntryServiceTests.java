44ab2cd53d6488ca581e4e9ca2f164551b37da4a
yanil01 <idoyanil45@gmail.com>
2026-04-18 21:32:04 +0545

    @Test @ParameterizedTest @CsvSource @ValueSource

--- journalApp/src/test/java/np/com/yanil/journalApp/service/UserEntryServiceTests.java
+++ journalApp/src/test/java/np/com/yanil/journalApp/service/UserEntryServiceTests.java
@@ -3,12 +3,18 @@ package np.com.yanil.journalApp.service;
 
 import np.com.yanil.journalApp.entity.UserEntry;
 import np.com.yanil.journalApp.repository.UserEntryRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
 public class UserEntryServiceTests {
@@ -16,9 +22,29 @@ public class UserEntryServiceTests {
     @Autowired
     UserEntryRepository userEntryRepository;
 
-    @Test
-    public void testFindByUserName(){
-        UserEntry userEntry = userEntryRepository.findByUserName("ram"); 
-        assertTrue(!userEntry.getJournalEntries().isEmpty());
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "ram",
+            "yanil02",
+            "jack02",
+            "hari"
+    })
+    public void testFindByUserName(String userName){
+        UserEntry userEntry = userEntryRepository.findByUserName(userName);
+        assertNotNull(userEntry);
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "ram",
+            "yanil02",
+            "jack02",
+            "hari"
+    })
+    public void testFindJournalByUserName(String userName){
+        UserEntry userEntry = userEntryRepository.findByUserName(userName);
+        assertFalse(userEntry.getJournalEntries().isEmpty());
     }
 }
